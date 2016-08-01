b0dfddddaa0ae756423c3f1a53c23027e5f1c7eb
zouooh1 <251788114@qq.com>
2016-08-01 13:39:33 +0800

    fix.

--- app/src/main/java/me/zouooh/slark/s/MainActivity.java
+++ app/src/main/java/me/zouooh/slark/s/MainActivity.java
@@ -2,8 +2,11 @@ package me.zouooh.slark.s;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import me.zouooh.slark.Logs;
 import me.zouooh.slark.Slark;
+import me.zouooh.slark.request.Request;
 import me.zouooh.slark.response.LogProgress;
 import me.zouooh.slark.response.TextResponse;
 
@@ -17,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
         Slark.DEBUG = true;
         Slark.init(getApplication());
         Slark.with(getApplication()).get("http://www.seqi360.com/app/gift").param("a", "b").progress(LogProgress
-                .obtain()).response(new TextResponse()).request();
+                .obtain()).response(new TextResponse(){
+            @Override
+            public void onRequestSuccess(Request request, String s) {
+                Toast.makeText(getBaseContext(),s,Toast.LENGTH_LONG).show();
+            }
+        }).request();
     }
 }
